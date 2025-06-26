6bd21a876d7104a16c1f849fb8b13db6bb870908
Rajdeepsavani <savanirajdeep5@gmail.com>
2025-06-26 19:08:52 -0400

    fix build error

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,6 @@ export default defineConfig(({ mode }) => ({
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: false,
-    minify: 'terser',
     rollupOptions: {
       output: {
         manualChunks: undefined,
