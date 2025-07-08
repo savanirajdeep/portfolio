7736763f222b55d5ef0a861c1f724774bf9a0f04
Rajdeepsavani <savanirajdeep5@gmail.com>
2025-07-08 16:29:24 -0400

    fixed ui changes

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: '/portfolio/',
+  base: '/portfolio',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
